793977700638da061b4414af75c15ca2376e88d8
Fernanda Fonseca <fernanda.fonseca3312@gmail.com>
2025-03-15 09:43:42 -0600

    arreglos

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let numeroSecreto = generarNumeroSecreto();
+let numeroSecreto = 0;
+let intentos = 0;
 
 console.log(numeroSecreto);
 
@@ -11,22 +12,47 @@ function asignarTextoElemento(elemento, texto) {
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
    
-    console.log(numeroSecreto);
     if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p','Acertaste el número');
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos == 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
+        //El usuario no acertó
         if(numeroDeUsuario > numeroSecreto) {
         asignarTextoElemento('p','El número secreto es menor');
         } else {
         asignarTextoElemento('p','El número secreto es mayor');
         }
+        intentos++;
+        limpiarCaja();
     }
     return;
 }
 
-function generarNumeroSecreto(){
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+    
+}
+
+function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
 }
 
-asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Indica un número del 1 al 10');
+function condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', 'Indica un número del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego() {
+    //limpiar la caja
+    limpiarCaja();
+    //indicar mensaje de intervalo de números
+    //generar el número aleatorio
+    //inicializar el número de intentos
+    condicionesIniciales();
+    //deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+}
+
+condicionesIniciales();
